035406ec3d2a2934968d5e864ccaa1e8e52d04b9
Nikita Luksha <nikita.luksha@softensity.com>
2021-12-20 20:08:23 +0300

    Split code and html

---
+++ Advanced/Advanced/Blazor/Split.razor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Advanced.Models;
+
+namespace Advanced.Blazor
+{
+    public partial class Split
+    {
+        [Inject]
+        public DataContext Contex { get; set; }
+
+        public IEnumerable<string> Names => Contex.People.Select(p => p.Firstname);
+    }
+}

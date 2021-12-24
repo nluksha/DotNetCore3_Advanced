a37db695e219c37f843596162710f706cf1e9052
Nikita Luksha <nikita.luksha@softensity.com>
2021-12-24 22:22:44 +0300

    form validation

--- Advanced/Advanced/Models/Person.cs
+++ Advanced/Advanced/Models/Person.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Advanced.Models
 {
     public class Person
     {
         public long PersonId { get; set; }
+        [Required(ErrorMessage = "Firstname is required")]
+        [MinLength(3, ErrorMessage = "Firstname must be 3 or more chracters")]
         public string Firstname { get; set; }
+        [Required(ErrorMessage = "Surname is required")]
+        [MinLength(3, ErrorMessage = "Surname must be 3 or more chracters")]
         public string Surname { get; set; }
+        [Required]
+        [Range(1, long.MaxValue, ErrorMessage = "Department must be selected")]
         public long DepartmentId { get; set; }
+        [Required]
+        [Range(1, long.MaxValue, ErrorMessage = "Location must be selected")]
         public long LocationId { get; set; }
 
         public Department Department { get; set; }
